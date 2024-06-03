62bcd1338f2b86e863478fe27deef21a1bf782c4
PAX-12 <joaquin.suarezvallejos@gmail.com>
2024-06-03 00:28:45 -0300

    some changes

--- js/js-global-blocks.js
+++ js/js-global-blocks.js
@@ -1,42 +1,41 @@
-/*
-	JS: Global CSS BLOCKS JS
-*/	
+// JAVASCRIPT
 
-	// JAVASCRIPT
+// COPYRIGHT YEAR (AUTOMATIC)
+let getyear = new Date().getFullYear();
+let getyeardiv = document.getElementById("year");
+getyeardiv.innerHTML = getyear;
 
-		// COPYRIGHT YEAR (AUTOMATIC)
-		let getyear = new Date().getFullYear();
-		let getyeardiv = document.getElementById('year');
-		getyeardiv.innerHTML = getyear;
+// HEADER FIXED
+function headerfixed() {
+  let getheaderdiv = document.getElementsByTagName("header")[0].classList;
+  if (document.documentElement.scrollTop > 20) {
+    getheaderdiv.add("fixed");
+  } 
+  else {
+    getheaderdiv.remove("fixed");
+  }
+}
+window.onscroll = headerfixed;
 
-
-		// HEADER FIXED
-		function headerfixed(){
-			let getheaderdiv = document.getElementsByTagName('header')[0].classList;
-			if(document.documentElement.scrollTop > 20){
-				getheaderdiv.add('fixed');
-			}else{
-				getheaderdiv.remove('fixed');
-			}
-		}
-		window.onscroll = headerfixed;
-
-
-	// JQUERY
-		$(document).ready(function(){
-
-
-			// NAV MENU MANAGEMENT 
-			$('a.showmenu').click(function(){$('.navigation').fadeIn(); $(this).hide(); $('a.hidemenu').fadeIn('slow'); });
-			$('a.hidemenu').click(function(){ $('.navigation').fadeOut(); $(this).hide(); $('a.showmenu').fadeIn('slow'); });
-			$('.navigation ul li').click(function(){
-				let getscreenwidth = $(window).width();
-				if(getscreenwidth < 1201){
-					$('.navigation').fadeOut();
-					$('a.showmenu').fadeIn('slow');
-					$('a.hidemenu').hide();
-				}
-			});
-
-
-		});
+// JQUERY
+$(document).ready(function () {
+  // NAV MENU MANAGEMENT
+  $("a.showmenu").click(function () {
+    $(".navigation").fadeIn();
+    $(this).hide();
+    $("a.hidemenu").fadeIn("slow");
+  });
+  $("a.hidemenu").click(function () {
+    $(".navigation").fadeOut();
+    $(this).hide();
+    $("a.showmenu").fadeIn("slow");
+  });
+  $(".navigation ul li").click(function () {
+    let getscreenwidth = $(window).width();
+    if (getscreenwidth < 1201) {
+      $(".navigation").fadeOut();
+      $("a.showmenu").fadeIn("slow");
+      $("a.hidemenu").hide();
+    }
+  });
+});
